7885688e88bb52bbd8ba0392a2f5af49a5da1029
Matt Maher <mattmaher0@gmail.com>
2018-05-13 21:45:49 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,7 +35,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for number_of_times 
+  for item in number_of_times 
   puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
